a77d0bec391d9b1ab75aa994f3f3204fec1f50d6
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-12-04 22:35:37 +0500

    update

--- config/settings.py
+++ config/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
