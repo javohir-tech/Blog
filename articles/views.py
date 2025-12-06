c8a79ada673b2adfda1bb86dc8618c9b029a549d
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-12-06 17:56:38 +0500

    artciles view create

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Article
+from django.views.generic import ListView , DetailView
+
+
+class ArticleListView(ListView) :
+    model = Article;
+    template_name = 'article_list.html'
 
 # Create your views here.
