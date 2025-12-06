8101497f503c725ce69ebfbdeae63f0ca9048823
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-12-06 18:23:58 +0500

    article update template create

--- articles/urls.py
+++ articles/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
     ArticleListView,
-    ArticleDetailView
+    ArticleDetailView,
+    ArticleUpdateView
 )
 
 urlpatterns = [
     path('' , ArticleListView.as_view() , name='articles'),
-    path('detail/<int:pk>/' , ArticleDetailView.as_view() , name='article_detail')
+    path('detail/<int:pk>/' , ArticleDetailView.as_view() , name='article_detail'),
+    path('detail/<int:pk>' , ArticleUpdateView.as_view() , name='article_update'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from .models import Article
-from django.views.generic import ListView , DetailView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import DeleteView, UpdateView, CreateView
 
 
-class ArticleListView(ListView) :
-    model = Article;
-    template_name = 'article_list.html'
+class ArticleListView(ListView):
+    model = Article
+    template_name = "article_list.html"
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "article_detail.html"
+    context_object_name = "article"
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    template_name = "article_update.html"
+    fields = ["title", "summary", "body", "photo"]
     
 
-class ArticleDetailView(DetailView) :
-    model = Article ;
-    template_name = "article_detail.html"
-    context_object_name = 'article'
-
-# Create your views here.
